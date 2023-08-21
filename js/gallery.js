b053516241ebf39cdc7d8e7dc37144793890c1c9
Paulo Goedert <goedert@MacBook-Pro-de-Paulo.local>
2023-08-21 10:57:57 -0300

    att

---
+++ js/gallery.js
@@ -0,0 +1,7 @@
+[...document.querySelectorAll('.column')].map(column => {
+	column.style.setProperty('--animation', 'slide');
+	column.style.setProperty('height', '200%');
+	column.innerHTML = column.innerHTML + column.innerHTML;
+});
+
+
